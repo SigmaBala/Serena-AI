23473d27bb2aea346506754de41e887ad74b6aa8
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-07 20:55:10 +0530

    Update AI names and IDs in config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+
 import os
 
-name = 'Serena AI'
-serena_id = 2030938170
+name = 'Luffy AI'
+shiki_id = 6380108080
 api_id = os.getenv('API_ID', None)
 api_hash = os.getenv('API_HASH', None)
 session = os.getenv('SESSION', None)
 password = os.getenv('password', None)
 username = os.getenv('username', None)
-db_url = os.getenv('DB_URL', "mongodb+srv://Shiki:xnp9czdVYgpT4KBE@shiki.smrp72r.mongodb.net/")
+db_url = os.getenv('DB_URL', None)
 chatbot_url = os.getenv('CHATBOT', None)
-char_id = "UkuPaK3" # this is serena is character id [ https://www.sakura.fm]
+char_id = "SIpz8R8"
