efaa930c2e4a410cb02784afabcf20575543bc2a
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-08 19:25:50 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,7 @@ serena = pyrogram.Client(
    name=config.name,
    api_id=config.api_id,
    api_hash=config.api_hash,
-   session_string=config.session,
+   bot_token=config.token,
    plugins=dict(root='main')
 )
 
