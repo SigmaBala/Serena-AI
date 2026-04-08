7b33c55e74dd3d5998852156b1d4923a3654dfe2
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-08 19:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 
 import os
 
-name = 'Luffy AI'
-luffy_id = 23444444
+name = 'Serena AI'
+serena_id = 23444444
 api_id = os.getenv('API_ID', None)
 api_hash = os.getenv('API_HASH', None)
-session = os.getenv('SESSION', None)
+bot_token = os.getenv('TOKEN', None)
 password = os.getenv('password', None)
 username = os.getenv('username', None)
 db_url = os.getenv('DB_URL', None)
