d7e8bf8af99cbed3f80de688d13cb9fa41f51156
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-08 20:44:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 name = 'Serena AI'
 serena_id = 23444444
-api_id = os.getenv('API_ID', None)
-api_hash = os.getenv('API_HASH', None)
-bot_token = os.getenv('TOKEN', None)
-password = os.getenv('password', None)
-username = os.getenv('username', None)
-db_url = os.getenv('DB_URL', None)
+api_id = os.getenv('API_ID', '13257951')
+api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
+bot_token = os.getenv('TOKEN', '8352844426:AAEW6QUz40nFIxIIDJq4Mlf7Hz4IOifSKy4')
+password = os.getenv('password', 'ro@4321$')
+username = os.getenv('username', 'BalaSigma')
+db_url = os.getenv('DB_URL', 'mongodb+srv://tofu:tofu@cluster0.42nsiw9.mongodb.net/?retryWrites=true&w=majority')
 chatbot_url = os.getenv('CHATBOT', None)
-char_id = "SIpz8R8"
+char_id = "JwOdxC9"
