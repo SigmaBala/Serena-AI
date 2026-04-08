21417b259a9e68b7e0ff650e3100a955d4b76fe8
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-08 19:18:49 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,66 +1,8 @@
-from aiohttp import web
+
 from main import serena
-
-import asyncio, aiohttp, logging, traceback
-
-WEB_URL = "https://chat-bot-2z78.onrender.com"
-WEB_SLLEP = 3*60
-
-log = logging.getLogger(__name__)
-
-
-routes = web.RouteTableDef()
-
-@routes.get('/', allow_head=True)
-async def hello(request):
-    return web.Response(text="Hello, world!")
-
-
-def web_server():
-    app = web.Application()
-    app.add_routes(routes)
-    return app
-
-
-
-
-async def keep_alive():
-    if WEB_URL:
-        while True:
-            await asyncio.sleep(WEB_SLLEP)
-            try:
-                async with aiohttp.ClientSession(
-                    timeout=aiohttp.ClientTimeout(total=10)
-                ) as session:
-                    async with session.get(WEB_URL) as resp:
-                        log.info(
-                            "Pinged {} with response: {}".format(
-                                WEB_URL, resp.status
-                            )
-                        )
-            except asyncio.TimeoutError:
-                log.warning("Couldn't connect to the site URL..!")
-            except Exception:
-                traceback.print_exc()
-                    
-
-BIND_ADDRESS = "0.0.0.0"
-PORT = 8080
-
-async def start_services():        
-        server = web.AppRunner(web_server())
-        await server.setup()
-        await web.TCPSite(server, BIND_ADDRESS, PORT).start()
-        log.info("Web Server Initialized Successfully")
-        log.info("=========== Service Startup Complete ===========")
-  
-        asyncio.create_task(keep_alive())
-        log.info("Keep Alive Service Started")
-        log.info("=========== Initializing Web Server ===========")
+import logging
         
 
 if __name__ == "__main__":
-     loop = asyncio.get_event_loop()
-     loop.run_until_complete(start_services())
      serena.run()
      log.info('Bot Started!')
