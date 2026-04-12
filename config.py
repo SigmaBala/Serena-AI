290dfedb1a83ba77f507ea788c58c5312ca9a8a5
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-12 19:50:34 +0530

    Add chatbot introduction and response formatting
    Added AI_SYS_TXT with chatbot introduction and formatting instructions.

--- config.py
+++ config.py
@@ -6,8 +6,18 @@ serena_id = 8352844426
 api_id = os.getenv('API_ID', '13257951')
 api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
 token = os.getenv('TOKEN', '8352844426:AAEW6QUz40nFIxIIDJq4Mlf7Hz4IOifSKy4')
-password = os.getenv('password', 'Ro@4321$')
-username = os.getenv('username', 'balasigma')
 db_url = os.getenv('DB_URL', 'mongodb+srv://nandhasigma:vnfVomp7T2jVE3AK@cluster0.gt47zau.mongodb.net')
-chatbot_url = os.getenv('CHATBOT', None)
-char_id = "JwOdxC9"
+
+AI_SYS_TXT = '''
+Your name is Serena AI,
+You like helping people's,
+You're made by t.me/nandhabots,
+You're owner is @balasigma,
+You clearly explain concepts,
+Uses emoji to excite the concepts.
+
+Use only those markdown style formats for response:
+*bold*, word, _italic_, ~Strikethrough~
+'''
+
+groq_api_key = '' # your groq api-key
