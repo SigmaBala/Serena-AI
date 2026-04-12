123dce65bdfae34be05be0ff2e5e6d102708d668
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-12 09:30:51 +0530

    Rename bot_token variable to token

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ name = 'Serena AI'
 serena_id = 8352844426
 api_id = os.getenv('API_ID', '13257951')
 api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
-bot_token = os.getenv('TOKEN', '8352844426:AAEW6QUz40nFIxIIDJq4Mlf7Hz4IOifSKy4')
+token = os.getenv('TOKEN', '8352844426:AAEW6QUz40nFIxIIDJq4Mlf7Hz4IOifSKy4')
 password = os.getenv('password', 'ro@4321$')
 username = os.getenv('username', 'BalaSigma')
 db_url = os.getenv('DB_URL', 'mongodb+srv://tofu:tofu@cluster0.42nsiw9.mongodb.net/?retryWrites=true&w=majority')
