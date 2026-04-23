da645f212cc0e2a449384c4cc0e45d9d3728e219
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-23 14:18:13 +0530

    Change token in config.py to a new value
    Updated the token environment variable for security.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ name = 'Serena AI'
 serena_id = 8352844426
 api_id = os.getenv('API_ID', '13257951')
 api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
-token = os.getenv('TOKEN', '8352844426:AAEW6QUz40nFIxIIDJq4Mlf7Hz4IOifSKy4')
+token = os.getenv('TOKEN', '8352844426:AAEdhGX5xoYyhXoXVtyJ7EuFimO_sl1TNGc')
 db_url = os.getenv('DB_URL', 'mongodb+srv://nandhasigma:vnfVomp7T2jVE3AK@cluster0.gt47zau.mongodb.net')
 
 AI_SYS_TXT = '''
