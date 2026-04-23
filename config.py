a51d54fa367908039b098973f36dd98f0b40339f
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-04-23 17:05:21 +0530

    Update token in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ name = 'Serena AI'
 serena_id = 8352844426
 api_id = os.getenv('API_ID', '13257951')
 api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
-token = os.getenv('TOKEN', '8352844426:AAEdhGX5xoYyhXoXVtyJ7EuFimO_sl1TNGc')
+token = os.getenv('TOKEN', '8352844426:AAHRu1of9IdBKyrRAYB1nD_MI_jiO8pJc6o')
 db_url = os.getenv('DB_URL', 'mongodb+srv://nandhasigma:vnfVomp7T2jVE3AK@cluster0.gt47zau.mongodb.net')
 
 AI_SYS_TXT = '''
