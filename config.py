bbb0016e8c4eba67621c1b03a269c61034d68fc4
BALASUBRAMANIAN <115382856+SigmaBala@users.noreply.github.com>
2026-05-08 19:29:24 +0530

    Update database URL in config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ serena_id = 8352844426
 api_id = os.getenv('API_ID', '13257951')
 api_hash = os.getenv('API_HASH', 'd8ea642aedb736d40035bc05f0cfd477')
 token = os.getenv('TOKEN', '8352844426:AAHRu1of9IdBKyrRAYB1nD_MI_jiO8pJc6o')
-db_url = os.getenv('DB_URL', 'mongodb+srv://nandhasigma:vnfVomp7T2jVE3AK@cluster0.gt47zau.mongodb.net')
+db_url = os.getenv('DB_URL', 'mongodb+srv://serena:serena123@cluster0.yeaa9qk.mongodb.net/?appName=Cluster0')
 
 developers = [5696053228, 1666544436, 2030938170]
 
